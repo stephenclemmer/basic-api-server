0c9013e6223ad081757ef2c5d90cfecb87245ef5
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-09-24 11:06:37 -0700

    Added config object to index.js, changed the DATABASE_URL in .env  to the one needed for heroku to deploy. Commented out the development DATABASE_URL in .env and commeneted out the development  sequelizeDatabase in the models index.

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,22 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite:memory'
   : process.env.DATABASE_URL;
 console.log(DATABASE_URL);
-const sequelizeDatabase = new Sequelize(DATABASE_URL, {});
+
+//   Uncomment for production; Comment out for Development
+const sequelizeDatabase = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+
+
+
+// Uncomment for development. Comment out for production
+// const sequelizeDatabase = new Sequelize(DATABASE_URL, {});
 
 const CarsModel = carsSchema(sequelizeDatabase, DataTypes);
 const GuitarsModel = guitarsSchema(sequelizeDatabase, DataTypes);
